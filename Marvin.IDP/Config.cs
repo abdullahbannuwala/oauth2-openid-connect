e4a807e5d601c63b95e9ec067e035bf5da7e13ea
abdullah <mabdullah@visiontechconsulting.ca>
2023-03-05 13:04:19 +0500

    IdentityServer  + authorization code flow  + Logout
    1. Configuring IdentityServer to log in with the authorization code flow
    2. Logging the authorization code flow
    3. Logout of our web application
    4. Redirecting after logout
    5. Returning Additional Claims From the UserInfo Endpoint

--- Marvin.IDP/Config.cs
+++ Marvin.IDP/Config.cs
@@ -18,5 +18,30 @@ public static class Config
 
     public static IEnumerable<Client> Clients =>
         new Client[] 
-            { };
+            {
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris =
+                    {
+                        "https://localhost:7184/signin-oidc"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "https://localhost:7184/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }, 
+                    RequireConsent = true
+                }
+            };
 }
